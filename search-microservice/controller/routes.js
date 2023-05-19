84a64bcca42a4ebab8586507427a2617b3f25101
AndreasB2B <99027014+AndreasB2B@users.noreply.github.com>
2023-05-19 23:21:01 +0200

    route 2

--- search-microservice/controller/routes.js
+++ search-microservice/controller/routes.js
@@ -3,13 +3,13 @@ import searchRepository from '../repository/searchRepository.js';
 
 const router = Router();
 
-router.get('/ms/searchservice/', (req, res) => {
+router.get('/ms/search/', (req, res) => {
     res.send("Welcome to the search API")
 })
 
-router.get('/ms/searchservice/allEvents', searchRepository.allEvents);
-router.post('/ms/searchservice/searchEvents', searchRepository.searchEvents);
-router.post('/ms/searchservice/nextEventPage', searchRepository.getNextEventPage);
-router.post('/ms/searchservice/eventsByCategory', searchRepository.getEventsByCategory);
+router.get('/ms/search/allEvents', searchRepository.allEvents);
+router.post('/ms/searchsearchEvents', searchRepository.searchEvents);
+router.post('/ms/searchnextEventPage', searchRepository.getNextEventPage);
+router.post('/ms/searcheventsByCategory', searchRepository.getEventsByCategory);
 
 export default router
